500e635615e2ef53bbbf71984530f256767ecf27
Lucas-191435 <lucasfernandosantos2001@gmail.com>
2026-05-24 09:46:42 -0300

    feat(throttler): add throttler module for rate limiting functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PokemonModule } from './modules/pokemon/pokemon.module';
@@ -8,12 +10,24 @@ import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
     PrismaModule,
     JwtGuardModule,
     AuthModule,
     PokemonModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
